bd20a3f68a092b62a183e6d64155e61a95619e01
Felipe Medeiros Castello Branco <felipe.m.c.b.fm@gmail.com>
2023-11-18 17:55:11 -0300

    adicionando mais algumas funçoes nas janelas

--- index.py
+++ index.py
@@ -3,7 +3,22 @@ from message_window import message_window
 from questions_window import questions_window
 
 if __name__ == "__main__":
+
+    # vars
+    theme1 = {'background': 'white', 'text-color': 'black', 'background-button': '#53079F'}
+    theme2 = {'background': 'white', 'text-color': 'black', 'background-button': '#b30036'}
+    player1 = 'Jogador 1'
+    player2 = 'Jogador 2'
+    questao = {'title':'What is the capital of France?', 
+               'alternatives':['Berlin', 'Madrid', 'Paris', 'Rome'],
+               'correct_answer':'awnser 1'}
+
+    
+    # begin code
     start = start_window()
     if start:
         message_window()
+        questions_window(player=player1, question=questao, theme=theme1, points=0)
+
+
 
--- questions_window.py
+++ questions_window.py
@@ -3,7 +3,10 @@ def questions_window(question, player, theme, points):
     sg.theme = theme
     letters = ['A', 'B', 'C', 'D']
     layout = [[sg.Button(f'{player}   |   Points: {points}'.upper(), size=(30, 3),button_color=('black', 'white'))],
+              [sg.Text('', background_color=theme['background'])],
+              [sg.Text('', background_color=theme['background'])],
               [sg.Text(f'{question["title"]}'.upper(), background_color=theme['background'], text_color=theme['text-color'])],
+              [sg.Text('', background_color=theme['background'])],
               [[sg.Button(f'{letters[counter]} - {alternative}'.upper(), 
                         size=(40, 3), key=f'{alternative}', button_color=theme['background-button']
                         )] 
@@ -20,26 +23,37 @@ def questions_window(question, player, theme, points):
     while True:
         event, value = window.read()
         print(event, value)
+
+        print(event.split()[0].upper())
+        print(question['correct_answer'].upper())
         if event==sg.WINDOW_CLOSED or event=='Exit':
             break
 
 
+        if event.split()[0].upper() == question['correct_answer'].upper():
+            # adicionar popup de acerto
+            return 1
+        
+        else:
+            # adicionar popup de erro
+            return -1
+
+
+
+
 if __name__=="__main__":
 
-
+    # vars
+    theme1 = {'background': 'white', 'text-color': 'black', 'background-button': '#53079F'}
+    theme2 = {'background': 'white', 'text-color': 'black', 'background-button': '#b30036'}
+    player1 = 'Jogador 1'
+    player2 = 'Jogador 2'
     questao = {'title':'What is the capital of France?', 
                'alternatives':['Berlin', 'Madrid', 'Paris', 'Rome'],
-               'correct_answer':'awnser 1'}
+               'correct_answer':'Paris'}
+
     
-    theme1 = {'background': 'white', 'text-color': 'black', 'background-button': '#53079F'}
+    questions_window(player=player1, question=questao, theme=theme1, points=0)
 
-    theme2 = {'background': 'white', 'text-color': 'black', 'background-button': '#b30036'}
-    
-    player1 = 'Jogador 1'
 
-    player2 = 'Jogador 2'
 
-    points = 0
-
-    questions_window(questao, player1, theme1, points)
-    questions_window(questao, player2, theme2, points)
